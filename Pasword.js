8913a4fb557a95829766fc01708bfa3511532e4c
depay1221 <depay067@gmail.com>
2025-04-17 05:21:09 +0700

    Create Pasword.js

---
+++ Pasword.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "depayj",
+
+  "description": "NIKA VERSION 6 Vvip", 
+
+  "lastUpdated": "12/04/2025",
+
+  "author": "Depay", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "6.0"
+
+}
