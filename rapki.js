30db7e12bb4ba4d66dd86ca7ddc682d09cd71889
depay1221 <depay067@gmail.com>
2025-04-18 07:08:30 +0700

    Create rapki.js

---
+++ rapki.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "rafkyxxx",
+
+  "description": "RapkiNoCounter", 
+
+  "lastUpdated": "12/04/2025",
+
+  "author": "RapkiGanteng", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "1.0"
+
+    }
